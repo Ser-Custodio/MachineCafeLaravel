b6d6a45009476959146a0f18d57219b724b2ff93
Sergio Custodio <sergio.custodio@Digital-Grenoble.local>
2018-01-16 11:10:59 +0100

    First commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tabBord', function () {
+    return view('tabBord');
+});
+
+Route::get('/boissons', function () {
+    return view('boissons');
+});
+
+Route::get('/ingredients', function () {
+    return view('ingredients');
+});
+
+Route::get('/monnayeur', function () {
+    return view('monnayeur');
+});
+
+Route::get('/recettes', function () {
+    return view('recettes');
+});
+
+Route::get('/ventes', function () {
+    return view('ventes');
+});
